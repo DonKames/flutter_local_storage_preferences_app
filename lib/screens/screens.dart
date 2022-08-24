878fb030f30fc5fa3be855b8af2c991d5c6e069d
Camilo Santander <camilo.santander1991@gmail.com>
2022-08-24 02:00:14 -0400

    Curso Flutter 197

---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_local_storage_preferences_app/screens/home_screen.dart';
+export 'package:flutter_local_storage_preferences_app/screens/settings_screen.dart';
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_local_storage_preferences_app/widgets/side_menu.dart';
